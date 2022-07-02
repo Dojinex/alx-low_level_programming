6adc9b23cb9f7b9b1f6c02c3ad88099ed43127ae
Dojinex <bitrusdauda9118@gmail.com>
2022-07-02 08:48:51 -0700

    task 101-print_comb4.c commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints possible combinations of two two-digit numbers.
+ * main - that prints all possible different combinations of three digits.
  *
  * Return: Always 0.
  */
@@ -10,10 +10,10 @@ int main(void)
 int digit1, digit2;
 for (digit1 = 0; digit1 < 9; digit1++)
 {
-for (digit2 = digit1 + 1; digit2++)
+for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+{
 putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
-
 if (digit1 == 8 && digit2 == 9)
 continue;
 putchar(',');
