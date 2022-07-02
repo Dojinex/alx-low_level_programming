601cc60ea2af7eaba5338b66937b580bda3ae110
Dojinex <bitrusdauda9118@gmail.com>
2022-07-02 09:38:21 -0700

    task 101-print_comb4.c commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,17 +7,30 @@
  */
 int main(void)
 {
-int digit1, digit2;
-for (digit1 = 0; digit1 < 9; digit1++)
+int digit1, digit2, digit3;
+for (digit1 = 48; digit1 < 58; digit1++)
 {
-for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+for (digit2 = digit1; digit2 < 58; digit2++)
 {
-putchar((digit1 % 10) + '0');
-putchar((digit2 % 10) + '0');
-if (digit1 == 8 && digit2 == 9)
+for (digit3 = digit2; digit3 < 58; digit3++)
+{
+if (digit1 == digit2 || digit2 == digit3 || digit1 == digit3)
+{
 continue;
+}
+putchar(digit1);
+putchar(digit2);
+putchar(digit3);
+if (digit1 == 55 && digit2 == 56 && digit3 == 57)
+{
+break;
+}
+else
+{
 putchar(',');
 putchar(' ');
+}
+}
 }
 }
 putchar('\n');
