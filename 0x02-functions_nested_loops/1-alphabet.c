8852ef35b8a671d958e8e4becf9827be283a95df
Dojinex <bitrusdauda9118@gmail.com>
2022-07-05 14:24:01 -0700

    task 1-alphabet.c commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase alphabets.
+ */
+void print_alphabet(void)
+{
+char j;
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
