2d9d6364260466f380507f90ec1d4e613d42cb2e
Dojinex <bitrusdauda9118@gmail.com>
2022-07-05 15:31:48 -0700

    task 6-abs.c commit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,11 +3,11 @@
 /**
  * _abs - returns absolute value of a number.
  * @n: parameter
- * Return: The absolute value of a numbe
+ * Return: The absolute value of a number.
  */
 int _abs(int n)
 {
-if (n >= 0)
+if (n < 0)
 n = -n;
 return (n);
 }
