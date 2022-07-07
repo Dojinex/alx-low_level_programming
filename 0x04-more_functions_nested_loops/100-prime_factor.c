d1ef3116b6585f4f021ed584e6d65cf8c14b2443
Dojinex <bitrusdauda9118@gmail.com>
2022-07-07 13:39:28 -0700

    task 100-prime_factor.c commit

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - prints prime factors of 612852475143
- *
- * Return: Always 0 (Success)
+ * main - Program that finds the largest prime number for 612,852,475,143
+ * Return: 0
  */
 int main(void)
 {
+long factor, num = 612852475143;
+int i, r;
+i = 2;
+factor = num;
+while (i < factor)
+{
+r = factor % i;
+while (r == 0)
+{
+factor /= i;
+r = factor % i;
+}
+++i;
+}
+printf("%li\n", factor);
 return (0);
 }
