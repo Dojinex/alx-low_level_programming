7c94fedf6180f7bc3c8777638811d619d9358547
Dojinex <bitrusdauda9118@gmail.com>
2022-07-07 09:44:19 -0700

    task 0-isupper.c commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isuper - check if a letter is uppercase character
+ * @x: The number to be checked
+ *
+ * Return: 1 for upper or 0 for any elsse
+ */
+int _isuper(int x)
+{
+if (x >= 65 && x <= 90)
+{
+return (1);
+}
+return (0);
+}
