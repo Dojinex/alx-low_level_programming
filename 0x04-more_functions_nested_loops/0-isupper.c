3809d54b4166659d05278ddc943f6f078f8347a7
Dojinex <bitrusdauda9118@gmail.com>
2022-07-07 10:23:05 -0700

    task 0-isupper.c commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
 /**
- * _isuper - check if a letter is uppercase character
- * @x: The number to be checked
- *
- * Return: 1 for upper or 0 for any elsse
+ * _isupper - a function that checks for uppercase character
+ * @c: input
+ * Return: 1 if c is uppercase, 0 otherwise
  */
-int _isuper(int x)
+int _isupper(int c)
 {
-if (x >= 65 && x <= 90)
-{
+if ((c >= 'A') && (c <= 'Z'))
 return (1);
-}
+else
 return (0);
 }
