3e9c14a7e59e6320f782e8a986989d63fc29de7e
Dojinex <bitrusdauda9118@gmail.com>
2022-07-16 04:08:32 -0700

    task 101-print_number.c commit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 
 int _putchar(char c);
+void print_number(int n);
+void print_buffer(char *b, int size);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -11,5 +13,8 @@ char *string_toupper(char *n);
 char *cap_string(char *n);
 char *leet(char *n);
 char *rot13(char *n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 
-#endif 
+#endif
+
