fc71bec99f20f33a5eaa78296ff2723b240556c1
Dojinex <bitrusdauda9118@gmail.com>
2022-07-17 12:16:53 -0700

    task 102-magic.c commit

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 int main(void)
-
 {
 int n;
 int a[5];
@@ -12,16 +11,15 @@ p = &n;
 
 	/**
 	 * write your line of code here...
-	 * Remember
+	 * Remember:
 	 * - you are not allowed to use a
-	 * - you are not allowed to modify
+	 * - you are not allowed to modify p
 	 * - only one statement
-	 * of code
+	 * - you are not allowed to code anything else than this line of code
 	 */
-
 *(p + 5) = 98;
-
-	   /* ...so that this prints 98\n */
+/* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
+
