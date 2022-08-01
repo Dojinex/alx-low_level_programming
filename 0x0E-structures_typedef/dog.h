23970248aa263590f1d0d6c21b42daf9739b774d
Dojinex <bitrusdauda9118@gmail.com>
2022-08-01 14:19:27 -0700

    task dog.h commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,29 +1,21 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * struct dog - A new type describing a dog.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
+ * struct dog - a new type
+ * @name : string
+ * @age : float
+ * @owner : string
  *
- * Description: The attributes of a dog.
+ * Description: a new type that contains dog name, age, and owner.
  */
 
-struct dog
+typedef struct dog
 {
-
 char *name;
 float age;
 char *owner;
-
-};
-
-/**
- * dog_t - Typedef for struct dog
- */
-
-typedef struct dog dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
