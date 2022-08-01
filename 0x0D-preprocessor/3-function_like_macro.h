f819aae37e154bfcbc3dc0593fef76bd2f54662f
Dojinex <bitrusdauda9118@gmail.com>
2022-08-01 12:17:08 -0700

    task 3-function_like_macro.h commit

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,13 +1,6 @@
-#ifndef FUNCTION_LIKE_MACRO_H
+#ifndef _function_like_macro_h_
+#define _function_like_macro_h_
 
-#define FUNCTION_LIKE_MACRO_H
+#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
 
-/**
- * File: 3-function_like_macro.h
- * Desc: Header file that defines a function-like macro ABS(x)
- * that computes the absolute value of a number x.
- */
-
-#define ABS(x) ((x) < 0 ? (-x) : (x))
-
-#endif
+#endif /* _function_like_macro_h_ */
