e6ca28e50cef9ed00b59114f1d0130d55f42f43a
Dojinex <bitrusdauda9118@gmail.com>
2022-08-04 05:16:42 -0700

    task 1-print_numbers.c commit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,15 +12,14 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-va_list valist;
-unsigned int i;
-if (separator == NULL)
-return;
-va_start(valist, n);
-for (i = 0; i < n; i++)
+va_list nums;
+unsigned int index;
+
+va_start(nums, n);
+for (index = 0; index < n; index++)
 {
-printf("%d", va_arg(valist, const unsigned int));
-if (i != (n - 1))
+printf("%d", va_arg(nums, int));
+if (index != (n - 1) && separator != NULL)
 printf("%s", separator)
 }
 printf("\n");
