bc5b7531c824409d7aabb07bc8c55da621c886e1
Dojinex <bitrusdauda9118@gmail.com>
2022-08-16 07:42:55 -0700

    task 0-print_listint.c commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_listint - prints all the elements of a linked list
@@ -8,13 +9,12 @@
 
 size_t print_listint(const listint_t *h)
 {
-const listint_t *cursor = h;
-size_t count = 0;
-while (cursor != NULL)
+size_t nodes = 0;
+while (h)
 {
-printf("%d\n", cursor->n);
-count += 1;
-cursor = cursor->next;
+nodes++;
+printf("%d\n", h->n);
+h = h->next;
 }
-return (count);
+return (nodes);
 }
