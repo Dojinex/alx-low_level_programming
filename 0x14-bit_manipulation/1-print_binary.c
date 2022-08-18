ac5497ad4a40e1514a09cefbf9e3a130c1277a62
Dojinex <bitrusdauda9118@gmail.com>
2022-08-18 07:45:53 -0700

    task 1-print_binary.c commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,24 +9,13 @@
 
 void print_binary(unsigned long int n)
 {
-int i = 0, count, k, temp;
-if (n == 0)
+if (n == 0 || n == 1)
 {
-printf("0");
-return;
+_putchar('0' + n);
 }
-temp = n;
-while (temp != 0)
-{
-i++;
-temp = temp >> 1;
-}
-for (count = i - 1; count >= 0; count--)
-{
-k = n >> count;
-if (k & 1)
-printf("1");
 else
-printf("0");
+{
+print_binary(n >> 1);
+_putchar('0' + (n & 1));
 }
 }
